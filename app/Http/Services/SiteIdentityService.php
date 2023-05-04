6136b1de604f13336a46b698cad52e6127529ebb
elhusseinn <elhusseinabdalaziz1@gmail.com>
2023-05-04 18:55:41 +0300

    added upload image logic to create site identity

--- app/Http/Services/SiteIdentityService.php
+++ app/Http/Services/SiteIdentityService.php
@@ -6,10 +6,12 @@ use App\DTOs\ModelDTO;
 use App\Http\RepoInterfaces\RepoRegisteryInterface;
 use App\Models\SiteIdentity;
 use App\Traits\DTOBuilder;
+use App\Traits\UploadImage;
 
 class SiteIdentityService
 {
     use DTOBuilder;
+    use UploadImage;
 
     private $registry;
     private $siteIdentityRepo;
@@ -21,11 +23,24 @@ class SiteIdentityService
 
     public function createSiteIdentity($siteIdentityData)
     {
+        if(array_key_exists('images', $siteIdentityData))
+        {
+            $siteIdentityData['images'] = $this->uploadImages($siteIdentityData['images']);
+        }
         $siteIdentityDTO = $this->createDTO($siteIdentityData);
+
         foreach ($siteIdentityDTO->getFillable() as $key => $value) {
             $siteIdentityDTO->fill[$key] = json_encode($value);
         }
        return $this->siteIdentityRepo->create($siteIdentityDTO);
+    }
+
+    public function uploadImages($images) // upload images for site identity
+    {
+        foreach ($images as $key => $image){
+           $images[$key] =  $this->uploadImage($image);
+        }
+        return $images;
     }
 
     public function createDTO($siteIdentityData):ModelDTO
