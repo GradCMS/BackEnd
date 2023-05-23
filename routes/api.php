750cb6fdfde393746903395acaf30ae4ba4642ea
elhusseinn <elhusseinabdalaziz1@gmail.com>
2023-05-24 00:12:02 +0300

    made the auth/me endpoint GET instead of POST

--- routes/api.php
+++ routes/api.php
@@ -70,7 +70,7 @@ Route::prefix('auth')->group(function (){
     Route::post('login', [AuthController::class, 'login']);
     Route::post('logout', [AuthController::class, 'logout']);
     Route::post('refresh', [AuthController::class, 'refresh']);
-    Route::post('me', [AuthController::class, 'me']);
+    Route::get('me', [AuthController::class, 'me']);
 
 });
 
